c80b562252ef45bffcffe403c4dbcf432e946a10
Daniel <endqwerty@gmail.com>
2014-12-09 13:58:41 -0800

    added assets back

---
+++ Assets/character.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character : MonoBehaviour
+{
+	public int hp;
+	public int cooldown;
+	public int damage;
+	private float lastAttack;
+	private bool hasEnemy;
+	// Use this for initialization
+	void Start ()
+	{
+		hasEnemy = false; //never has an enemy to start with;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+			if (hasEnemy) {//TODO: Detect enemy
+					if ((Time.time - lastAttack) >= cooldown) {
+							//TODO: attack
+							lastAttack = Time.time; //if attack set time to now
+					}
+			}
+	}
+	//attack if there is an Enemy
+	void Attack ()
+	{
+		
+	}
+
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public float speed;
+	
+	private Vector2 currentDirection = Vector2.zero;
+	// Update is called once per frame
+	void Update () {
+
+		//if (currentDirection.Equals(Vector2.zero))
+		//{
+			Vector2 inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+			if (!inputDirection.Equals(Vector2.zero))
+			{
+				currentDirection = inputDirection;
+				this.rigidbody2D.velocity = currentDirection * speed;
+			}
+		//}
+	}
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		currentDirection = Vector3.zero;
+		this.rigidbody2D.velocity = Vector3.zero;
+	}
+}
