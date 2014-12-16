6d4925355380f0d444bd6e0f8b114433174ff318
nccanlas <ncanlas@uci.edu>
2014-12-15 17:48:16 -0800

    grass isFilled works

--- Assets/Scripts/Grass.cs
+++ Assets/Scripts/Grass.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class Grass : MonoBehaviour {
-
-    void OnMouseUpAsButton() {
-        // Is there something to build?
-        if (BuildMenu.cur != null) {
-            // Build it
-            Instantiate(BuildMenu.cur.gameObject, transform.position, Quaternion.identity);
-            SunCollect.score -= BuildMenu.cur.price;
-            BuildMenu.cur = null;
-        }
-    }
+	
+	public bool isFilled = false;
+	
+	void OnMouseUpAsButton() {
+		// Is there something to build?
+		if (BuildMenu.cur != null && !isFilled) {
+			// Build it
+			Instantiate(BuildMenu.cur.gameObject, transform.position, Quaternion.identity);
+			SunCollect.score -= BuildMenu.cur.price;
+			BuildMenu.cur = null;
+			isFilled = true;
+		}
+	}
 }
